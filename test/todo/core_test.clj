1ba2d91eabcd7e9f2e19cad1731a88067b016e4e
Gideon Farrell <gideon@converge.io>
2017-08-07 22:05:51 +0200

    add some basic tests

--- test/todo/core_test.clj
+++ test/todo/core_test.clj
@@ -2,6 +2,12 @@
   (:require [clojure.test :refer :all]
             [todo.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest adding-todos
+    (testing "add-todo adds a todo to a list"
+      (let [result (add-todo ["wake up"] "make breakfast")]
+          (is (= result #{ "make breakfast" "wake up" })))))
+
+(deftest deleting-todos
+    (testing "del-todo removes a todo from a list"
+        (let [result (del-todo ["one" "two"] "two")]
+            (is (= result #{"one"})))))
